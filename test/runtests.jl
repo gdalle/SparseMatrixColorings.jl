614285ea4acb8fd8519e3cb3dd634ab298f576af
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-17 12:43:25 +0200

    Quality tests on 1.10 only

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,17 +6,19 @@ using SparseMatrixColorings
 using Test
 
 @testset verbose = true "SparseMatrixColorings" begin
-    @testset verbose = true "Code quality" begin
-        @testset "Aqua" begin
-            Aqua.test_all(SparseMatrixColorings)
-        end
-        @testset "JET" begin
-            JET.test_package(SparseMatrixColorings)
-        end
-        @testset "JuliaFormatter" begin
-            @test JuliaFormatter.format(
-                SparseMatrixColorings; verbose=false, overwrite=false
-            )
+    if VERSION >= v"1.10"
+        @testset verbose = true "Code quality" begin
+            @testset "Aqua" begin
+                Aqua.test_all(SparseMatrixColorings)
+            end
+            @testset "JET" begin
+                JET.test_package(SparseMatrixColorings)
+            end
+            @testset "JuliaFormatter" begin
+                @test JuliaFormatter.format(
+                    SparseMatrixColorings; verbose=false, overwrite=false
+                )
+            end
         end
     end
     @testset "Doctests" begin
