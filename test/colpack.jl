f684475811cbcdb766d487d37d533a9dfc951f21
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-06-04 18:13:17 +0200

    Only test correctness

--- test/colpack.jl
+++ test/colpack.jl
@@ -1,5 +1,4 @@
-using Chairmarks
-using ColPack: ColPack
+using ColPack: ColPack, ColPackColoring
 using LinearAlgebra
 using SparseMatrixColorings
 using SparseMatrixColorings: NaturalOrder
@@ -9,36 +8,53 @@ using Test
 
 rng = StableRNG(63)
 
-function my_symmetric_coloring(H)
-    return symmetric_coloring(H, GreedyColoringAlgorithm(NaturalOrder()))
-end
+my_column_coloring(J) = column_coloring(J, GreedyColoringAlgorithm(NaturalOrder()))
+my_row_coloring(J) = row_coloring(J, GreedyColoringAlgorithm(NaturalOrder()))
+my_symmetric_coloring(H) = symmetric_coloring(H, GreedyColoringAlgorithm(NaturalOrder()))
 
-function colpack_symmetric_coloring(H)
-    method = ColPack.star_coloring()
-    ordering = ColPack.natural_ordering()
-    coloring = ColPack.ColPackColoring(H, method, ordering)
+function colpack_column_coloring(J)
+    A = ColPack.matrix2adjmatrix(J; partition_by_rows=false)
+    coloring = ColPackColoring(A, ColPack.d1_coloring(), ColPack.natural_ordering())
     return ColPack.get_colors(coloring)
 end
 
-n_values = floor.(Int, 10 .^ (1:5))
-p_values(n) = (2:5:min(n, 30)) ./ n
+function colpack_row_coloring(J)
+    A = ColPack.matrix2adjmatrix(J; partition_by_rows=true)
+    coloring = ColPackColoring(A, ColPack.d1_coloring(), ColPack.natural_ordering())
+    return ColPack.get_colors(coloring)
+end
 
-@testset verbose = true "Star coloring" begin
-    @testset "Correctness" begin
+function colpack_symmetric_coloring(H)
+    coloring = ColPackColoring(H, ColPack.star_coloring(), ColPack.natural_ordering())
+    return ColPack.get_colors(coloring)
+end
+
+n_values = floor.(Int, 10 .^ (1:3))
+p_values(n) = (2:4:min(n, 20)) ./ n
+
+@testset verbose = true "Correctness" begin
+    @testset "Column coloring" begin
+        @testset "n=$n - p=$p" for n in n_values, p in p_values(n)
+            J = sprand(rng, n, n + 1, p)
+            color1 = my_column_coloring(J)
+            color2 = colpack_column_coloring(J)
+            @test color1 == color2
+        end
+    end
+    @testset "Row coloring" begin
+        @testset "n=$n - p=$p" for n in n_values, p in p_values(n)
+            J = sprand(rng, n, n + 1, p)
+            color1 = my_row_coloring(J)
+            color2 = colpack_row_coloring(J)
+            @test color1 == color2
+        end
+    end
+    @testset "Symmetric coloring" begin
         @testset "n=$n - p=$p" for n in n_values, p in p_values(n)
             H = sparse(Symmetric(sprand(rng, n, n, p)))
             color1 = my_symmetric_coloring(H)
             color2 = colpack_symmetric_coloring(H)
             @test color1 == color2
-        end
-    end
-    @testset "Performance" begin
-        @testset "n=$n - p=$p" for n in n_values, p in p_values(n)
-            bench1 = @be sparse(Symmetric(sprand(rng, n, n, p))) my_symmetric_coloring
-            bench2 = @be sparse(Symmetric(sprand(rng, n, n, p))) colpack_symmetric_coloring
-            time1 = minimum(bench1).time
-            time2 = minimum(bench2).time
-            @test time2 > time1
         end
     end
 end;
