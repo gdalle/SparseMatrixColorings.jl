c540a9442cad75e2876850f0f33f6937802af6ee
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-08-09 18:54:47 +0200

    Noref

--- src/decompression.jl
+++ src/decompression.jl
@@ -6,10 +6,6 @@ Decompress `B` out-of-place into a new matrix `A`, given a coloring `result` of 
 # See also
 
 - [`AbstractColoringResult`](@ref)
-
-# References
-
-> [_Efficient Computation of Sparse Hessians Using Coloring and Automatic Differentiation_](https://pubsonline.informs.org/doi/abs/10.1287/ijoc.1080.0286), Gebremedhin et al. (2009), Figures 2 and 3
 """
 function decompress(B::AbstractMatrix{R}, result::AbstractColoringResult) where {R<:Real}
     S = get_matrix(result)
