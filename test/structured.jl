f3531afb1ebe39c2a274c790cc977730a3596566
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-10-03 09:56:54 +0200

    Remove cycle range tests

--- test/structured.jl
+++ test/structured.jl
@@ -6,17 +6,6 @@ using SparseMatrixColorings
 using SparseMatrixColorings: cycle_range
 using Test
 
-@testset "Utils" begin
-    @test cycle_range(2, 3) == [1, 2, 1]
-    @test cycle_range(2, 4) == [1, 2, 1, 2]
-    @test cycle_range(2, 5) == [1, 2, 1, 2, 1]
-    @test cycle_range(3, 5) == [1, 2, 3, 1, 2]
-    @test cycle_range(3, 6) == [1, 2, 3, 1, 2, 3]
-    @test cycle_range(2, 1) == [1]
-    @test cycle_range(3, 1) == [1]
-    @test cycle_range(3, 2) == [1, 2]
-end;
-
 @testset "Diagonal" begin
     for n in (1, 2, 10, 100)
         A = Diagonal(rand(n))
