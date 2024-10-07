000b2c49d17c7a794e5d2d09b3565f525a0e345d
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-10-07 19:41:30 +0200

    No abstract

--- src/order.jl
+++ src/order.jl
@@ -167,7 +167,7 @@ function change_degree_and_push_into_bucket!(
 end
 
 function vertices(
-    g::AbstractAdjacencyGraph, ::DynamicDegreeBasedOrder{degtype,direction}
+    g::AdjacencyGraph, ::DynamicDegreeBasedOrder{degtype,direction}
 ) where {degtype,direction}
     # Initialize degrees
     if (degtype == :back && direction == :up) || (degtype == :forward && direction == :down)
