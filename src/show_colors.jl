a1486135b1b100b2ef1490744641349281e7aa94
adrhill <adrian.hill@mailbox.org>
2024-10-08 22:29:12 +0200

    Better docstring

--- src/show_colors.jl
+++ src/show_colors.jl
@@ -12,7 +12,8 @@ Return an image visualizing an [`AbstractColoringResult`](@ref).
 
 ## Keyword arguments
 * `colorscheme`: colors used for non-zero matrix entries. Defaults to the Wong colorscheme.
-* `background`: color used for zero matrix entries and pad. Defaults to a transparent background.
+    This can be a vector of Colorants or a predefined scheme from [ColorSchemes.jl](https://github.com/JuliaGraphics/ColorSchemes.jl/blob/master/src/ColorSchemes.jl).
+* `background::Colorant`: color used for zero matrix entries and pad. Defaults to `RGBA(0,0,0,0)`, a transparent background.
 * `scale::Int`: scale the size of matrix entries to `scale × scale` pixels. Defaults to `1`. 
 * `pad::Int`: padding between matrix entries. Defaults to `0`. 
 
