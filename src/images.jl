0630575898be40e1fa818af13564021c15ff0881
adrhill <adrian.hill@mailbox.org>
2024-10-08 19:53:10 +0200

    Better docstring

--- src/images.jl
+++ src/images.jl
@@ -9,7 +9,7 @@ Return an image visualizing an [`AbstractColoringResult`](@ref).
     Using this function requires loading [Images.jl](https://github.com/JuliaImages/Images.jl).
 
 ## Keyword arguments
-* `colorscheme`: colors used for non-zero matrix entries. 
+* `colorscheme`: colors used for non-zero matrix entries. Defaults to the Wong colorscheme.
 * `background`: color used for zero matrix entries and padding. Defaults to a transparent background.
 * `scale::Int`: scale the size of matrix entries to `scale × scale` pixels. Defaults to `1`. 
 * `padding::Int`: padding between matrix entries. Defaults to `0`. 
