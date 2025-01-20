ccc27c4a1cb62094f8a96ca9fa45179f686ac3e4
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-01-20 08:46:31 +0100

    Generic seed type

--- src/order.jl
+++ src/order.jl
@@ -38,14 +38,14 @@ end
 Instance of [`AbstractOrder`](@ref) which sorts vertices using a random permutation, generated from `rng` with a given `seed`.
 
 - If `seed = nothing`, the `rng` will never be re-seeded. Therefore, two consecutive calls to `vertices(g, order)` will give different results.
-- If `seed isa Integer`, the `rng` will be re-seeded before each sample. Therefore, two consecutive calls to `vertices(g, order)` will give the same result.
+- Otherwise, the `rng` will be re-seeded before each sample. Therefore, two consecutive calls to `vertices(g, order)` will give the same result.
 
 !!! warning
     Do not use a seed with the `default_rng()`, otherwise you will affect the global state of your program.
     If you need reproducibility, create a new `rng` specifically for your `RandomOrder`.
     The package [StableRNGs.jl](https://github.com/JuliaRandom/StableRNGs.jl) offers random number generators whose behavior is stable across Julia versions.
 """
-struct RandomOrder{R<:AbstractRNG,S<:Union{Integer,Nothing}} <: AbstractOrder
+struct RandomOrder{R<:AbstractRNG,S} <: AbstractOrder
     rng::R
     seed::S
 end
