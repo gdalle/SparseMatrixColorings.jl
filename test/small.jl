6832e7f2716c25b7ea05d312b202443f0e9d42d0
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-02-12 19:32:25 +0100

    Add rectangle example (#177)
    * Add rectangle example
    
    * Add arrowhead
    
    * Remove comment
    
    * Add triangle

--- test/small.jl
+++ test/small.jl
@@ -8,6 +8,7 @@ using SparseMatrixColorings:
     what_fig_61,
     efficient_fig_1,
     efficient_fig_4
+using StableRNGs
 using Test
 
 @testset "Column coloring & decompression" begin
@@ -85,3 +86,75 @@ end;
         test_coloring_decompression(A0, problem, algo; B0, color0)
     end
 end;
+
+@testset "Bidirectional coloring" begin
+    problem = ColoringProblem(; structure=:nonsymmetric, partition=:bidirectional)
+    order = RandomOrder(StableRNG(0), 0)
+
+    @testset "Triangle" begin
+        A = sparse([1 1 0; 0 1 1; 1 0 1])
+
+        result = coloring(
+            A, problem, GreedyColoringAlgorithm{:direct}(; postprocessing=true)
+        )
+        @test ncolors(result) == 3
+
+        result = coloring(
+            A, problem, GreedyColoringAlgorithm{:substitution}(; postprocessing=true)
+        )
+        @test ncolors(result) == 3
+    end
+
+    @testset "Rectangle" begin
+        A = spzeros(Bool, 10, 20)
+        A[:, 1] .= 1
+        A[:, end] .= 1
+        A[1, :] .= 1
+        A[end, :] .= 1
+
+        result = coloring(
+            A, problem, GreedyColoringAlgorithm{:direct}(order; postprocessing=false)
+        )
+        @test ncolors(result) == 6  # two more than necessary
+        result = coloring(
+            A, problem, GreedyColoringAlgorithm{:direct}(order; postprocessing=true)
+        )
+        @test ncolors(result) == 4  # optimal number
+
+        result = coloring(
+            A, problem, GreedyColoringAlgorithm{:substitution}(order; postprocessing=false)
+        )
+        @test ncolors(result) == 6  # two more than necessary
+        result = coloring(
+            A, problem, GreedyColoringAlgorithm{:substitution}(order; postprocessing=true)
+        )
+        @test ncolors(result) == 4  # optimal number
+    end
+
+    @testset "Arrowhead" begin
+        A = spzeros(Bool, 10, 10)
+        for i in axes(A, 1)
+            A[1, i] = 1
+            A[i, 1] = 1
+            A[i, i] = 1
+        end
+
+        result = coloring(
+            A, problem, GreedyColoringAlgorithm{:direct}(order; postprocessing=true)
+        )
+        @test ncolors(coloring(A, problem, GreedyColoringAlgorithm{:substitution}(order))) <
+            ncolors(coloring(A, problem, GreedyColoringAlgorithm{:direct}(order)))
+
+        @test ncolors(
+            coloring(
+                A,
+                problem,
+                GreedyColoringAlgorithm{:substitution}(order; postprocessing=true),
+            ),
+        ) < ncolors(
+            coloring(
+                A, problem, GreedyColoringAlgorithm{:direct}(order; postprocessing=true)
+            ),
+        )
+    end
+end
