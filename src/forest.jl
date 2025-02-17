be89ee8557394745740aff67a03500bfe7352a58
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2025-02-17 14:10:12 -0600

    Apply suggestions from code review
    Co-authored-by: Guillaume Dalle <22795598+gdalle@users.noreply.github.com>

--- src/forest.jl
+++ src/forest.jl
@@ -20,12 +20,12 @@ function Base.push!(forest::Forest{T}, edge::Tuple{T,T}) where {T<:Integer}
     forest.counter += 1
     forest.intmap[edge] = forest.counter
     forest.ntrees += one(T)
-    return edge
+    return forest
 end
 
 function _find_root!(parents::Vector{T}, index_edge::T) where {T<:Integer}
-    @inbounds p = parents[index_edge]
-    @inbounds if parents[p] != p
+    p = parents[index_edge]
+    if parents[p] != p
         parents[index_edge] = p = _find_root!(parents, p)
     end
     return p
@@ -38,15 +38,15 @@ end
 function root_union!(forest::Forest{T}, index_edge1::T, index_edge2::T) where {T<:Integer}
     parents = forest.parents
     rks = forest.ranks
-    @inbounds rank1 = rks[index_edge1]
-    @inbounds rank2 = rks[index_edge2]
+    rank1 = rks[index_edge1]
+    rank2 = rks[index_edge2]
 
     if rank1 < rank2
         index_edge1, index_edge2 = index_edge2, index_edge1
     elseif rank1 == rank2
         rks[index_edge1] += one(T)
     end
-    @inbounds parents[index_edge2] = index_edge1
+    parents[index_edge2] = index_edge1
     forest.ntrees -= one(T)
     return nothing
 end
