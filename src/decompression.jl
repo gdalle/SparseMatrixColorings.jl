bbd9261f6c0285ecf9b84fe71a639f8bebebd678
Alexis Montoison <Alexis.Montoison@amd.com>
2026-09-07 14:37:27 -0600

    Add a missing assert

--- src/decompression.jl
+++ src/decompression.jl
@@ -721,6 +721,7 @@ function decompress!(
     uplo::Symbol=:F,
 ) where {R<:Real}
     check_compatible_pattern(A, result.ag, uplo)
+    @assert result.decompression_uplo == uplo || result.decompression_uplo == :F
     decompress_csc!(nonzeros(A), A.colptr, B, result, uplo)
     return A
 end
